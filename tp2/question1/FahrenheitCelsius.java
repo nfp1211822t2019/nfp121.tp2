3a9ffb0532c4c04cf1a6fbb00157379c724f1756
Hala Moussallem <hala.mooussallem@isae.edu.lb>
2019-03-23 16:31:56 +0200

    tp2

--- tp2/question1/FahrenheitCelsius.java
+++ tp2/question1/FahrenheitCelsius.java
@@ -1,42 +1,45 @@
 package question1;
 
 /**
-
- * DÃ©crivez votre classe FahrenheitCelsius ici.
+ * Décrivez votre classe FahrenheitCelsius ici.
  * 
  * @author (votre nom)
- * @version (un numÃ©ro de version ou une date)
+ * @version (un numéro de version ou une date)
  */
 public class FahrenheitCelsius {
 
-	/**
-	 * le point d'entrÃ©e de cette application, dont le commentaire est Ã 
-	 * complÃ©ter
-	 * 
-	 * @param args
-	 *            ...
-	 */
-	public static void main(String[] args) {
-		// pour tous les paramÃ¨tres de la ligne de commande
-		int fahrenheit = 0;
-		float celsius = 0;
-		System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); // ligne,
-																				// format
-																				// imposÃ©s
-																				
-	}
+    /**
+     * le point d'entrée de cette application, dont le commentaire est à
+     * compléter
+     * 
+     * @param args
+     *            ...
+     */
+    public static void main(String[] args) {
+        // pour tous les paramètres de la ligne de commande
+        for(int i=0;i<args.length;i++){
+        int fahrenheit = Integer.parseInt(args[i]);
+        float celsius = fahrenheitEnCelsius(fahrenheit);
+        System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); // ligne,
+        }                                               // format
+                                                                                // imposés
+                                                                                
+    }
 
-	/**
-	 * la mÃ©thode Ã  complÃ©ter.
-	 * 
-	 * @param f
-	 *            la valeur en degrÃ© Fahrenheit
-	 * @return la conversion en degrÃ© Celsius
-	 */
-	public static float fahrenheitEnCelsius(int f) {
-
-		return 0.F; // Ã  complÃ©ter en remplaÃ§ant ce return 0.F par la fonction
-					// de conversion
-	}
+    /**
+     * la méthode à compléter.
+     * 
+     * @param f
+     *            la valeur en degré Fahrenheit
+     * @return la conversion en degré Celsius
+     */
+    public static float fahrenheitEnCelsius(int f) {
+        float C;
+        C=(float)(((f - 32)*5.0)/9.0);
+        C=(float)(((int)(C*10.0))/10.0);
+       
+        return C; // à compléter en remplaçant ce return 0.F par la fonction
+                    // de conversion
+    }
 
 }
--- tp2/question3/IHMFahrenheit.java
+++ tp2/question3/IHMFahrenheit.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+
 public class IHMFahrenheit extends JFrame implements ActionListener{
   private JTextField entree = new JTextField( 6 );
   /** Le bouton de conversion. */
@@ -32,8 +33,11 @@ public class IHMFahrenheit extends JFrame implements ActionListener{
    */
   public void actionPerformed( ActionEvent ae ){
     try{
-      int fahrenheit = 0; // valeur est une String et doit être convertie en entier, voir java.lang.Integer méthode parseInt (--> try/catch)
-      float celsius = 0F; // à compléter, en appelant la méthode ad'hoc de la question2 
+        
+        String t=entree.getText();
+      int fahrenheit =Integer.parseInt(t); // valeur est une String et doit être convertie en entier, voir java.lang.Integer méthode parseInt (--> try/catch)
+      float celsius = FahrenheitCelsius.fahrenheitEnCelsius(fahrenheit); // à compléter, en appelant la méthode ad'hoc de la question2 
+      if(celsius<-273.1){celsius=-273.1f;}
       // un test ici pour le zéro absolu (-273.1)
 
       sortie.setText( Float.toString( celsius));
